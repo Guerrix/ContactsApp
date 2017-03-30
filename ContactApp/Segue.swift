45bb1e346b5e1467b6ff30bc63d9180340291f1c
Jesus Guerra <jguerrix@gmail.com>
2017-03-30 00:30:48 -0700

    Add Segue Struct
    To have a better definition for segue names

---
+++ ContactApp/Segue.swift
@@ -0,0 +1,13 @@
+//
+//  Segue.swift
+//  ContactApp
+//
+//  Created by Jesus Guerra on 3/30/17.
+//  Copyright © 2017 Jesus Guerra. All rights reserved.
+//
+
+import Foundation
+
+struct Segue {
+  static let contactDetail = "contactDetail"
+}
