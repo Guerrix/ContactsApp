4d634a70d3f4e6d2f417ef5c890a6b4d5ee816e2
Jesus Guerra <jguerrix@gmail.com>
2017-03-29 21:42:25 -0700

    Add contact cell skeleton

---
+++ ContactApp/ContacTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ContacTableViewCell.swift
+//  ContactApp
+//
+//  Created by Jesus Guerra on 3/29/17.
+//  Copyright © 2017 Jesus Guerra. All rights reserved.
+//
+
+import UIKit
+import DataKit
+
+class ContacTableViewCell: BaseTableViewCell {
+
+    @IBOutlet weak var contactAddress: UILabel!
+    @IBOutlet weak var contactName: UILabel!
+
+    func configure(withContact contact: Contact) {
+
+    }
+
+}
